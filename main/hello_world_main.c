ee565c6bbc87950e78c384a791d1c06e3c612cb0
Sam Velasquez <atrox.lapa@hotmail.com>
2020-07-12 17:55:35 -0300

    added wifi information

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,8 +1,41 @@
-#include <stdio.h> //unknown bug on intellisense. Just ignore it.
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <sys/time.h>
-#include <hd44780.h>
+/*
+
+void app_main()
+{
+    hd44780_init(&lcd);
+    wifi_init();
+
+    
+}
+*/
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "hd44780.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define EXAMPLE_ESP_WIFI_SSID "Virus"
+#define EXAMPLE_ESP_WIFI_PASS "cegonha13"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+
+static char IPADDR[16];
+static char GWADDR[16];
+static char MKADDR[16];
 
 hd44780_t lcd = {
     .write_cb = NULL,
@@ -17,48 +50,154 @@ hd44780_t lcd = {
         .d7 = GPIO_NUM_21,
         .bl = HD44780_NOT_USED}};
 
-static uint32_t get_time_sec()
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec;
-}
-
-static const uint8_t char_data[] = {
-    0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
-    0x1f, 0x11, 0x0a, 0x04, 0x0a, 0x11, 0x1f, 0x00};
-
-void lcd_test(void *pvParameters)
-{
-    hd44780_clear(&lcd);
-    hd44780_upload_character(&lcd, 0, char_data);
-    hd44780_upload_character(&lcd, 1, char_data + 8);
-
-    hd44780_gotoxy(&lcd, 0, 0);
-    hd44780_puts(&lcd, "\x08 Hello world!");
-    hd44780_gotoxy(&lcd, 0, 1);
-    hd44780_puts(&lcd, "\x09 ");
-
-    char time[16];
-
-    while (1)
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
-        hd44780_gotoxy(&lcd, 2, 1);
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        /*
+            in the next lines we got some important info about WiFi
+            connection, such as IP, Gateway, Mask and SSID;
 
-        snprintf(time, 7, "%u  ", get_time_sec());
-        time[sizeof(time) - 1] = 0;
+        */
+        sprintf(IPADDR, IPSTR, IP2STR(&event->ip_info.ip));
+        sprintf(GWADDR, IPSTR, IP2STR(&event->ip_info.gw));
+        sprintf(MKADDR, IPSTR, IP2STR(&event->ip_info.netmask));
 
-        hd44780_puts(&lcd, time);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
 
-void app_main()
+void wifi_init_sta(void)
 {
-    ESP_ERROR_CHECK(hd44780_init(&lcd));
-    hd44780_puts(&lcd, "Testing LCD");
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    s_wifi_event_group = xEventGroupCreate();
 
-    xTaskCreate(lcd_test, "lcd_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .pmf_cfg = {
+                .capable = true,
+                .required = false},
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+void show_wifi_info()
+{
+    for (;;)
+    {
+        hd44780_clear(&lcd);
+        hd44780_gotoxy(&lcd, 0, 0);
+        hd44780_puts(&lcd, "IP:");
+        hd44780_gotoxy(&lcd, 3, 0);
+        hd44780_puts(&lcd, IPADDR);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        hd44780_clear(&lcd);
+        hd44780_gotoxy(&lcd, 0, 0);
+        hd44780_puts(&lcd, "GW:");
+        hd44780_gotoxy(&lcd, 3, 0);
+        hd44780_puts(&lcd, GWADDR);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        hd44780_clear(&lcd);
+        hd44780_gotoxy(&lcd, 0, 0);
+        hd44780_puts(&lcd, "MK:");
+        hd44780_gotoxy(&lcd, 3, 0);
+        hd44780_puts(&lcd, MKADDR);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void)
+{
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    hd44780_init(&lcd);
+
+    xTaskCreate(show_wifi_info, "show_wifi_info", configMINIMAL_STACK_SIZE * 5, NULL, 8, NULL);
 }
